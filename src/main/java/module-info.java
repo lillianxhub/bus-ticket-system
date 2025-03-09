e80d9630f29656b5fd6dbdafc4c99e31f63e6c71
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2025-03-10 02:51:43 +0700

    Initialize

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.bus_ticket_system {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.bus_ticket_system to javafx.fxml;
+    exports com.bus_ticket_system;
+}
